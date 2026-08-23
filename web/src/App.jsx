b57954911c00f6051929f459e0906c8d32071fcd
cnb.dQQuPkFehNA <2vqemw6IethKTW8eKNknKW+cnb.dQQuPkFehNA@noreply.cnb.cool>
2026-08-23 11:16:55 +0800

    fix(web): 补回 useRef import,修复黑屏(useReveal 删除时连带误删)

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { NavLink, Outlet, useLocation } from 'react-router-dom'
 import { getAccounts, getCurrentAccount, setCurrentAccount, getIcons } from './api'
 import { AccountContext, IconsContext } from './context'
